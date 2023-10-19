b496236c01c42d5189e3645190e4c0957050aa49
Ayomide-Oyeneye <nickbelson11@gmail.com>
2023-10-19 04:07:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Semester2-exam-Altschool
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Ayomide-Oyeneye/Semester2-exam-Altschool)
---
+++ src/routes/homepage.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "../style.css";
+function Homepage() {
+  return (
+    <>
+      <h1 className="homepage">Welcome to my home page</h1>
+    </>
+  );
+}
+export default Homepage;
